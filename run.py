b976b7b74661418787184f16e39edad3008b8c93
Dylan <Dylanmcmahon187@gmail.com>
2023-03-13 19:17:20 +0000

    Add color to game using 'Colorama'

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import random
 from dice_art import DICE_ART
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 
 def print_dice(first_dice, second_dice):
@@ -19,15 +22,16 @@ def roll_dice():
     total = dice1 + dice2
 
     while True:
-        response = input("Please use 'r' to roll your dice\n")
+        response = input(Fore.CYAN + "Please use 'r' to roll your dice\n")
         if response == "r":
-            print("Dice Rolling....\n")
+            print(Fore.YELLOW + "Dice Rolling....\n")
             print_dice(dice1, dice2)
-            print(f"you rolled a total of {total}\n")
+            print(f"{Fore.YELLOW}you rolled a total of {total}\n")
             game_logic(total)
             break
         else:
-            print("You have to enter 'r' to roll your dice/nTry again\n!")
+            print(Fore.YELLOW + "You have to enter 'r' to roll your dice")
+            print(Fore.YELLOW + "Try Again!\n")
 
 
 def game_logic(total):
@@ -35,13 +39,13 @@ def game_logic(total):
     The Logic behind the game
     """
     if total == 7 or total == 11:
-        print("You win!\n")
+        print(Fore.GREEN + "You win!\n")
         restart()
     elif total == 2 or total == 3 or total == 12:
-        print("You lost!\n")
+        print(Fore.RED + "You lost!\n")
         restart()
     else:
-        print(f"You have hit the Point\nYour point is {total}")
+        print(f"{Fore.YELLOW}You have hit the Point\nYour point is {total}")
         roll_dice()
 
 
@@ -50,26 +54,28 @@ def introduction():
     """
     Introduction asking if you need instructions or not
     """
-    print("============================================\n")
-    print("█░█ █▀▀ █░░ █░░ █▀█   ▄▀█ █▄░█ █▀▄")
-    print("█▀█ ██▄ █▄▄ █▄▄ █▄█   █▀█ █░▀█ █▄▀\n")
-    print("█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█")
-    print("▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█\n")
-    print("▀█▀ █▀▀ █▀█ █▀▄▀█ █ █▄░█ ▄▀█ █░░   █▀▄ █ █▀▀ █▀▀")
-    print("░█░ ██▄ █▀▄ █░▀░█ █ █░▀█ █▀█ █▄▄   █▄▀ █ █▄▄ ██▄\n")
-    print("Do you want to practice your skills before you hit the casino?\n")
-    print("If so you have came to the right place\n")
-    print("============================================\n")
+    print(Fore.BLUE + "\n======================================\n")
+    print(Fore.BLUE + "█░█ █▀▀ █░░ █░░ █▀█   ▄▀█ █▄░█ █▀▄")
+    print(Fore.RED +  "█▀█ ██▄ █▄▄ █▄▄ █▄█   █▀█ █░▀█ █▄▀\n")
+    print(Fore.BLUE + "█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█")
+    print(Fore.RED +  "▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█\n")
+    print(Fore.BLUE + "▀█▀ █▀▀ █▀█ █▀▄▀█ █ █▄░█ ▄▀█ █░░   █▀▄ █ █▀▀ █▀▀")
+    print(Fore.RED +  "░█░ ██▄ █▀▄ █░▀░█ █ █░▀█ █▀█ █▄▄   █▄▀ █ █▄▄ ██▄\n")
+    print(Fore.GREEN + "Do you want to practice your skills before you hit\n")
+    print(Fore.BLUE + "▀█▀ █░█ █▀▀   █▀▀ ▄▀█ █▀ █ █▄░█ █▀█")
+    print(Fore.RED +  "░█░ █▀█ ██▄   █▄▄ █▀█ ▄█ █ █░▀█ █▄█\n")
+    print(Fore.GREEN + "If so you have came to the right place\n")
+    print(Fore.BLUE + "==========================================\n")
     player_name()
-    response = input("Do you know how to play? (yes/no)\n")
+    response = input(Fore.CYAN + "Do you know how to play? (yes/no)\n")
     if response == "yes":
-        print("Get ready your game is comming right up.....\n")
+        print(Fore.GREEN + "Get ready your game is comming right up.....\n")
         roll_dice()
     elif response == "no":
-        print("\nLoading instructions....\n")
+        print(Fore.GREEN + "\nLoading instructions....\n")
         how_to_play()
     else:
-        print("\nPlease enter 'yes' or 'no'\nTry again\n")
+        print(Fore.GREEN + "\nPlease enter 'yes' or 'no'\nTry again\n")
         introduction()
 
 
@@ -78,34 +84,40 @@ def how_to_play():
     Shows you how to play the game and starts or resets
     the game depending on the answer
     """
-    print("The player rolls the dice and adds the numbers together.\n")
-    print("If the total is 7 or 11, the player wins.\n")
-    print("If the total is 2, 3, or 12, the player loses.\n")
-    print("If the total is any other number (4, 5, 6, 8, 9, or 10),\n")
-    print("That number becomes the 'point'.\n")
-    print("The player then continues to roll the dice until they\n")
-    print("Either roll the'point' again and win, or they roll a 7 and lose.\n")
+    print(Fore.YELLOW + "The player rolls the dice,\n")
+    print(Fore.YELLOW + "and adds the numbers together.\n")
+    print(Fore.YELLOW + "If the total is 7 or 11 the player wins.\n")
+    print(Fore.YELLOW + "If the total is 2, 3, or 12, the player loses.\n")
+    print(Fore.YELLOW + "If the total is any other number")
+    print(Fore.YELLOW + "(4, 5, 6, 8, 9, or 10),\n")
+    print(Fore.YELLOW + "That number becomes the 'point'.\n")
+    print(Fore.YELLOW + "The player then continues")
+    print(Fore.YELLOW + "to roll the dice until they\n")
+    print(Fore.YELLOW + "Either roll the'point' again and win,")
+    print(Fore.YELLOW + "or they roll a 7 and lose.\n")
     while True:  # Loop to ask the same question if there is a error
-        response = input("\nDo you want to start the game? (yes/no)\n")
+        response = input(Fore.CYAN + "\nDo you want to start the game?(y/n)\n")
         if response == "yes":
-            print("Best of luck your game is coming right up.....\n")
+            print(Fore.GREEN + "Best of luck your game is coming right up..\n")
             roll_dice()
             break
         elif response == "no":
-            print("You just want to watch the world burn...Restarting game\n")
+            print(Fore.GREEN + "You just want to watch the world burn...")
+            print(Fore.GREEN + "Restarting game\n")
             restart()
             break
         else:
-            print("\nPlease enter 'yes' or 'no'.\n")
+            print(Fore.RED + "\nPlease enter 'yes' or 'no'.\n")
 
 
 def player_name():
     """
     For setting the players name.
     """
-    print("------------------\n")
-    name = input("What is your name?\n\n------------------\n\n")
-    print(f"\nWelcome to the game {name}\n")
+    print(Fore.BLUE + "------------------\n")
+    name = input(Fore.CYAN + "What is your name?\n\n")
+    print(Fore.BLUE + "\n------------------\n")
+    print(f"{Fore.GREEN}\nWelcome to the game {name}\n")
     player_age()
 
 
@@ -113,30 +125,30 @@ def player_age():
     """
     To check if the player is old enough to play
     """
-    age = int(input("What age are you?\n"))
+    age = int(input(Fore.CYAN + "What age are you?\n\n"))
     if age >= 21:
-        print("You meet the age requirements\n")
+        print(Fore.GREEN + "You meet the age requirements\n")
     elif age < 21:
-        print("I am sorry but you are not old enough to play\n")
-        print("Error...Restarting Terminal Dice\n")
+        print(Fore.RED + "I am sorry but you are not old enough to play\n")
+        print(Fore.RED + "Error...Restarting Terminal Dice\n")
         introduction()
     else:
-        print("Please enter your age\n")
+        print(Fore.GREEN + "Please enter your age\n")
 
 
 def restart():
     """
     For restarting the game
     """
-    response = input("Do you want to play again? 'yes' or 'no'\n")
+    response = input(Fore.CYAN + "Do you want to play again? 'yes' or 'no'\n")
     if response == "yes":
-        print("Get ready your Game is comming right up.....\n")
+        print(Fore.GREEN + "Get ready your Game is comming right up.....\n")
         roll_dice()
     elif response == "no":
-        print("\nLoading instructions....\n")
+        print(Fore.GREEN + "\nLoading instructions....\n")
         how_to_play()
     else:
-        print("\nPlease enter yes or no\nTry again\n")
+        print(Fore.GREEN + "\nPlease enter yes or no\nTry again\n")
         introduction()
 
 
